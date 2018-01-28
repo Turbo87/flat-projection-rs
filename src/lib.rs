7d9b23d0a33dfc2f160304a76a08ddcddf5b8afa
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-01-28 19:04:34 +0100

    Add API documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,75 @@
+
+//! Fast geodesic distance calculations via flat surface projection.
+//!
+//! The [`FlatProjection`] struct can by used to project geographical
+//! coordinates from [WGS84] into a cartesian coordinate system.
+//! In the projected form approximated distance and bearing calculations
+//! can be performed much faster than on a sphere. The precision of these
+//! calculations is very precise for distances up to about 500 km.
+//!
+//! [`FlatProjection`]: struct.FlatProjection.html
+//! [WGS84]: https://en.wikipedia.org/wiki/World_Geodetic_System
+//!
+//! ## Example
+//!
+//! ```
+//! # #[macro_use]
+//! # extern crate assert_approx_eq;
+//! # extern crate flat_projection;
+//! #
+//! # use flat_projection::FlatProjection;
+//! #
+//! # fn main() {
+//! let (lon1, lat1) = (6.186389, 50.823194);
+//! let (lon2, lat2) = (6.953333, 51.301389);
+//!
+//! let proj = FlatProjection::new(51.05);
+//!
+//! let p1 = proj.project(lon1, lat1);
+//! let p2 = proj.project(lon2, lat2);
+//!
+//! let distance = p1.distance(&p2);
+//! // -> 75.648 km
+//! #
+//! # assert_approx_eq!(distance, 75.635_595, 0.02);
+//! # }
+//! ```
+//!
+//! ## Related
+//!
+//! - [cheap-ruler] – Similar calculations in JavaScripts
+//! - [cheap-ruler-cpp] – C++ port of cheap-ruler
+//!
+//! [cheap-ruler]: https://github.com/mapbox/cheap-ruler
+//! [cheap-ruler-cpp]: https://github.com/mapbox/cheap-ruler-cpp
+
+/// Projection from [WGS84] to a cartesian coordinate system for fast
+/// geodesic approximations.
+///
+/// [WGS84]: https://en.wikipedia.org/wiki/World_Geodetic_System
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate assert_approx_eq;
+/// # extern crate flat_projection;
+/// #
+/// # use flat_projection::FlatProjection;
+/// #
+/// # fn main() {
+/// let (lon1, lat1) = (6.186389, 50.823194);
+/// let (lon2, lat2) = (6.953333, 51.301389);
+///
+/// let proj = FlatProjection::new(51.05);
+///
+/// let p1 = proj.project(lon1, lat1);
+/// let p2 = proj.project(lon2, lat2);
+///
+/// let distance = p1.distance(&p2);
+/// // -> 75.648 km
+/// #
+/// # assert_approx_eq!(distance, 75.635_595, 0.02);
+/// # }
+/// ```
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub struct FlatProjection {
     kx: f64,
@@ -5,6 +77,14 @@ pub struct FlatProjection {
 }
 
 impl FlatProjection {
+    /// Creates a new `FlatProjection` instance that will work best around
+    /// the given latitude.
+    ///
+    /// ```
+    /// # use flat_projection::FlatProjection;
+    /// #
+    /// let proj = FlatProjection::new(51.);
+    /// ```
     pub fn new(latitude: f64) -> FlatProjection {
         // see https://github.com/mapbox/cheap-ruler/
         let cos = latitude.to_radians().cos();
@@ -20,6 +100,20 @@ impl FlatProjection {
         FlatProjection { kx, ky }
     }
 
+    /// Converts a longitude and latitude (in degrees) to a [`FlatPoint`]
+    /// instance that can be used for fast geodesic approximations.
+    ///
+    /// [`FlatPoint`]: struct.FlatPoint.html
+    ///
+    /// ```
+    /// # use flat_projection::FlatProjection;
+    /// #
+    /// let (lon, lat) = (6.186389, 50.823194);
+    ///
+    /// let proj = FlatProjection::new(51.);
+    ///
+    /// let flat_point = proj.project(lon, lat);
+    /// ```
     pub fn project(&self, longitude: f64, latitude: f64) -> FlatPoint {
         let x = longitude * self.kx;
         let y = latitude * self.ky;
@@ -28,6 +122,20 @@ impl FlatProjection {
     }
 }
 
+/// Representation of a geographical point on Earth as projected
+/// by a [`FlatProjection`] instance.
+///
+/// [`FlatProjection`]: struct.FlatProjection.html
+///
+/// ```
+/// # use flat_projection::FlatProjection;
+/// #
+/// let (lon, lat) = (6.186389, 50.823194);
+///
+/// let proj = FlatProjection::new(51.);
+///
+/// let flat_point = proj.project(lon, lat);
+/// ```
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub struct FlatPoint {
     x: f64,
@@ -35,16 +143,95 @@ pub struct FlatPoint {
 }
 
 impl FlatPoint {
+    /// Calculates the approximate distance in kilometers from
+    /// this `FlatPoint` to another.
+    ///
+    /// ```
+    /// # #[macro_use]
+    /// # extern crate assert_approx_eq;
+    /// # extern crate flat_projection;
+    /// #
+    /// # use flat_projection::FlatProjection;
+    /// #
+    /// # fn main() {
+    /// let (lon1, lat1) = (6.186389, 50.823194);
+    /// let (lon2, lat2) = (6.953333, 51.301389);
+    ///
+    /// let proj = FlatProjection::new(51.05);
+    ///
+    /// let p1 = proj.project(lon1, lat1);
+    /// let p2 = proj.project(lon2, lat2);
+    ///
+    /// let distance = p1.distance(&p2);
+    /// // -> 75.648 km
+    /// #
+    /// # assert_approx_eq!(distance, 75.635_595, 0.02);
+    /// # }
+    /// ```
     pub fn distance(&self, other: &FlatPoint) -> f64 {
         let (dx, dy) = self.delta(other);
         distance(dx, dy)
     }
 
+    /// Calculates the approximate average bearing in degrees
+    /// between -180 and 180 from this `FlatPoint` to another.
+    ///
+    /// ```
+    /// # #[macro_use]
+    /// # extern crate assert_approx_eq;
+    /// # extern crate flat_projection;
+    /// #
+    /// # use flat_projection::FlatProjection;
+    /// #
+    /// # fn main() {
+    /// let (lon1, lat1) = (6.186389, 50.823194);
+    /// let (lon2, lat2) = (6.953333, 51.301389);
+    ///
+    /// let proj = FlatProjection::new(51.05);
+    ///
+    /// let p1 = proj.project(lon1, lat1);
+    /// let p2 = proj.project(lon2, lat2);
+    ///
+    /// let bearing = p1.bearing(&p2);
+    /// // -> 45.3°
+    /// #
+    /// # assert_approx_eq!(bearing, 45.312, 0.001);
+    /// # }
+    /// ```
     pub fn bearing(&self, other: &FlatPoint) -> f64 {
         let (dx, dy) = self.delta(other);
         bearing(dx, dy)
     }
 
+    /// Calculates the approximate [`distance`] and average [`bearing`]
+    /// from this `FlatPoint` to another.
+    ///
+    /// [`distance`]: #method.distance
+    /// [`bearing`]: #method.bearing
+    ///
+    /// ```
+    /// # #[macro_use]
+    /// # extern crate assert_approx_eq;
+    /// # extern crate flat_projection;
+    /// #
+    /// # use flat_projection::FlatProjection;
+    /// #
+    /// # fn main() {
+    /// let (lon1, lat1) = (6.186389, 50.823194);
+    /// let (lon2, lat2) = (6.953333, 51.301389);
+    ///
+    /// let proj = FlatProjection::new(51.05);
+    ///
+    /// let p1 = proj.project(lon1, lat1);
+    /// let p2 = proj.project(lon2, lat2);
+    ///
+    /// let (distance, bearing) = p1.distance_bearing(&p2);
+    /// // -> 75.648 km and 45.3°
+    /// #
+    /// # assert_approx_eq!(distance, 75.635_595, 0.02);
+    /// # assert_approx_eq!(bearing, 45.312, 0.001);
+    /// # }
+    /// ```
     pub fn distance_bearing(&self, other: &FlatPoint) -> (f64, f64) {
         let (dx, dy) = self.delta(other);
         (distance(dx, dy), bearing(dx, dy))
