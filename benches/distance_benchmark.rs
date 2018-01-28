3822d0c1e7e44043319ad48c41e59543bfb17a05
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-01-28 21:44:39 +0100

    Add benchmark using `criterion`

---
+++ benches/distance_benchmark.rs
@@ -0,0 +1,25 @@
+#[macro_use]
+extern crate criterion;
+extern crate flat_projection;
+
+use criterion::Criterion;
+use flat_projection::FlatProjection;
+
+const AACHEN: (f64, f64) = (6.186389, 50.823194);
+const MEIERSBERG: (f64, f64) = (6.953333, 51.301389);
+
+fn flat_distance(p1: (f64, f64), p2: (f64, f64)) -> f64 {
+    let proj = FlatProjection::new((p1.1 + p2.1) / 2.);
+
+    let flat1 = proj.project(p1.0, p1.1);
+    let flat2 = proj.project(p2.0, p2.1);
+
+    flat1.distance(&flat2)
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("flat", |b| b.iter(|| flat_distance(AACHEN, MEIERSBERG)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
