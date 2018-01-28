a9c9f62b12753a78b4deee7bae21e7c7e2aa5415
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-01-28 21:55:24 +0100

    Add Haversine distance to benchmark

--- benches/distance_benchmark.rs
+++ benches/distance_benchmark.rs
@@ -17,8 +17,20 @@ fn flat_distance(p1: (f64, f64), p2: (f64, f64)) -> f64 {
     flat1.distance(&flat2)
 }
 
+fn haversine_distance(p1: (f64, f64), p2: (f64, f64)) -> f64 {
+    let dlat = (p2.1 - p1.1).to_radians();
+    let dlon = (p2.0 - p1.0).to_radians();
+    let lat1 = p1.1.to_radians();
+    let lat2 = p2.1.to_radians();
+
+    let a = (dlat / 2.).sin().powi(2) + (dlon / 2.).sin().powi(2) * lat1.cos() * lat2.cos();
+
+    2. * a.sqrt().atan2((1. - a).sqrt()) * 6371008.8
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("flat", |b| b.iter(|| flat_distance(AACHEN, MEIERSBERG)));
+    c.bench_function("haversine", |b| b.iter(|| haversine_distance(AACHEN, MEIERSBERG)));
 }
 
 criterion_group!(benches, criterion_benchmark);
